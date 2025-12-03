76830c6534dd8b631c089a77205d689c4b22d404
Avertocle <avertocle@gmail.com>
2025-12-03 14:16:28 +0530

    aoc 2025 day02

--- aoc/2025/day02/day02_test.go
+++ aoc/2025/day02/day02_test.go
@@ -1,14 +1,15 @@
 package day02
 
 import (
-	"github.com/avertocle/contests/aoc/testz"
 	"testing"
+
+	"github.com/avertocle/contests/aoc/testz"
 )
 
 func TestAll(t *testing.T) {
 	testCases := [][]string{
-		{"input_small.txt", "0", "0"},
-		{"input_final.txt", "0", "0"},
+		{"input_small.txt", "1227775554", "12599655151"},
+		{"input_final.txt", "4174379265", "20942028255"},
 	}
 	testz.Execute(t, testCases, ParseInput, []func() string{SolveP1, SolveP2})
 }
